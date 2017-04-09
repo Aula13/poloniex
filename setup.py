757fbbc0d495153274bfe0c3e134ce2c0750e729
enricobacis <enrico.bacis@gmail.com>
2017-04-09 02:48:00 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open('README.rst') as README:
+    long_description = README.read()
+    long_description = long_description[long_description.index('Description'):]
+
+setup(name='poloniex',
+      version='0.0.1',
+      description='Poloniex API client for humans',
+      long_description=long_description,
+      url='http://github.com/Aula13/poloniex',
+      author='Enrico Bacis, Daniele Ciriello',
+      author_email='enrico.bacis@gmail.com, ciriello.daniele@gmail.com',
+      license='MIT',
+      packages=['poloniex'],
+      install_requires=['requests'],
+      keywords='poloniex cryptocurrency cryptocurrencies api client bitcoin'
+)
