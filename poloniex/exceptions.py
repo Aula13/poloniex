797caebda1d4e6dfa77cae3b18853108d3860742
dnlcrl <ciriello.daniele@gmail.com>
2017-04-10 22:29:56 +0200

    pep8

--- poloniex/exceptions.py
+++ poloniex/exceptions.py
@@ -2,13 +2,16 @@ class PoloniexException(Exception):
     """Generic Poloniex Exception."""
     pass
 
+
 class PoloniexCredentialsException(PoloniexException, RuntimeError):
     """Missing or wrong credentials while using Trading API."""
     pass
 
+
 class PoloniexCommandException(PoloniexException, RuntimeError):
     """Error in command execution."""
     pass
+
 
 class PoloniexInvalidParametersException(PoloniexException, RuntimeError):
     """Wrong parameters while using Private API."""
