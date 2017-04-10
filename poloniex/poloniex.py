945dcf6239c74c7bd2c36d6093d74ec0ecd81c96
dnlcrl <ciriello.daniele@gmail.com>
2017-04-10 13:35:50 +0200

    adds templates for all unimplemented functions

--- poloniex/poloniex.py
+++ poloniex/poloniex.py
@@ -166,6 +166,109 @@ class Poloniex(PoloniexPublic):
         and "end" GET parameters."""
         PoloniexPublic.returnTradeHistory(self, currencyPair, start, end)
 
+    def returnOrderTrades(self):
+        """Returns all trades involving a given order, specified by the
+        "orderNumber" POST parameter. If no trades for the order have occurred
+        or you specify an order that does not belong to you, you will receive
+        an error. """
+        raise NotImplementedError
+
+    def buy(self):
+        """Places a limit buy order in a given market. Required POST parameters
+         are "currencyPair", "rate", and "amount". If successful, the method
+         will return the order number.
+        You may optionally set "fillOrKill", "immediateOrCancel", "postOnly"
+        to 1. A fill-or-kill order will either fill in its entirety or be
+        completely aborted. An immediate-or-cancel order can be partially or
+        completely filled, but any portion of the order that cannot be filled
+        immediately will be canceled rather than left on the order book.
+        A post-only order will only be placed if no portion of it fills
+        immediately; this guarantees you will never pay the taker fee on any
+        part of the order that fills."""
+        raise NotImplementedError
+
+    def sell(self):
+        """Places a sell order in a given market. Parameters and output are
+        the same as for the buy method."""
+        raise NotImplementedError
+
+    def cancelOrder(self):
+        """Cancels an order you have placed in a given market. Required POST
+        parameter is "orderNumber"."""
+        return
+
+    def moveOrder(self):
+        """Cancels an order and places a new one of the same type in a single
+        atomic transaction, meaning either both operations will succeed or both
+         will fail. Required POST parameters are "orderNumber" and "rate"; you
+         may optionally specify "amount" if you wish to change the amount of
+         the new order. "postOnly" or "immediateOrCancel" may be specified for
+         exchange orders, but will have no effect on margin orders. """
+        raise NotImplementedError
+
+    def withdraw(self):
+        """Immediately places a withdrawal for a given currency, with no email
+        confirmation. In order to use this method, the withdrawal privilege
+        must be enabled for your API key. Required POST parameters are
+        "currency", "amount", and "address". For XMR withdrawals, you may
+        optionally specify "paymentId"."""
+        raise NotImplementedError
+
+    def returnFeeInfo(self):
+        """If you are enrolled in the maker-taker fee schedule, returns your
+        current trading fees and trailing 30-day volume in BTC. This
+        information is updated once every 24 hours."""
+        raise NotImplementedError
+
+    def returnAvailableAccountBalances(self):
+        """Returns your balances sorted by account. You may optionally specify
+        the "account" POST parameter if you wish to fetch only the balances of
+        one account. Please note that balances in your margin account may not
+        be accessible if you have any open margin positions or orders."""
+        raise NotImplementedError
+
+    def returnTradableBalances(self):
+        """Returns your current tradable balances for each currency in each
+        market for which margin trading is enabled. Please note that these
+        balances may vary continually with market conditions."""
+        raise NotImplementedError
+
+    def transferBalance(self):
+        """Transfers funds from one account to another (e.g. from your exchange
+         account to your margin account). Required POST parameters are
+         "currency", "amount", "fromAccount", and "toAccount"."""
+        raise NotImplementedError
+
+    def returnMarginAccountSummary(self):
+        """Returns a summary of your entire margin account. This is the same
+        information you will find in the Margin Account section of the Margin
+        Trading page, under the Markets list. """
+        raise NotImplementedError
+
+    def marginBuy(self):
+        """Places a margin buy order in a given market. Required POST
+        parameters are "currencyPair", "rate", and "amount". You may optionally
+         specify a maximum lending rate using the "lendingRate" parameter.
+         If successful, the method will return the order number and any trades
+         immediately resulting from your order."""
+        raise NotImplementedError
+
+    def marginSell(self):
+        """Places a margin sell order in a given market. Parameters and output
+        are the same as for the marginBuy method."""
+        raise NotImplementedError
+
+    def getMarginPosition(self):
+        """Returns information about your margin position in a given market,
+        specified by the "currencyPair" POST parameter. You may set
+        "currencyPair" to "all" if you wish to fetch all of your margin
+        positions at once. If you have no margin position in the specified
+        market, "type" will be set to "none". "liquidationPrice" is an
+        estimate, and does not necessarily represent the price at which an
+        actual forced liquidation will occur. If you have no liquidation
+        price, the value will be -1. """
+        raise NotImplementedError
+
     def closeMarginPosition(self, currencyPair):
         """Closes your margin position in a given market (specified by the
         "currencyPair" POST parameter) using a market order. This call will
