6b4f369bcd3d66fdbc93d1eaca70ede3b273f565
dnlcrl <ciriello.daniele@gmail.com>
2017-04-28 19:10:22 +0200

    poloniex_push.py

--- poloniex/poloniex_push.py
+++ poloniex/poloniex_push.py
@@ -1,13 +1,14 @@
 from __future__ import print_function
 
 import sys as _sys
+import six as _six
 
 from autobahn.asyncio.wamp import ApplicationSession as _ApplicationSession
 from autobahn.asyncio.wamp import ApplicationRunner as _ApplicationRunner
 from trollius import coroutine as _coroutine, From as _From
 
 
-class PoloniexPush(_ApplicationSession):
+class Session(_ApplicationSession):
 
     def onConnect(self):
         self.join(self.config.realm)
@@ -15,49 +16,42 @@ class PoloniexPush(_ApplicationSession):
     @_coroutine
     def onJoin(self, details):
         def onTicker(*args, **kwargs):
-            if 'onEvent' in self.config.extra:
-                if kwargs:
-                    self.config.extra['onEvent'](args, kwargs)
-                else:
-                    self.config.extra['onEvent'](args)
-            else:
-                print("Event received:", args, kwargs)
-        if _sys.version_info >= (3, 0):
-            topic = self.config.extra['topic']
-        else:
-            topic = unicode(self.config.extra['topic'])
+            self.config.extra['backFunction'](args, kwargs)
+
+        topic = _six.u(self.config.extra['topic'])
 
         yield _From(self.subscribe(onTicker, topic))
 
 
-class PushMain():
+class PoloniexPush():
     '''
-    Alternatively, you can instantiate the PushMain class and call the feed function to use this as a module
+    Alternatively, you can instantiate the PoloniexPush class and call the feed function to use this as a module
     i.e.
-    >> from wss_client import PushMain
-    >> ticker_feed = PushMain()
+    >> from wss_client import PoloniexPush
+    >> ticker_feed = PoloniexPush('ticker')
     >> print(ticker_feed.feed())
-
-    You will need to implement it as thread / async function if you want your program to do anything other than
-    spit out the websocket feed forever and ever - unfortunately beyond my limited expertise
     '''
 
-    def __init__(self, topic='ticker'):
+    def __init__(self, topic, backFunction):
         self.topic = topic
+        self.backFunction = backFunction
 
     def feed(self):
-        runner = _ApplicationRunner(u"wss://api.poloniex.com:443", u"realm1", extra={'topic': self.topic})
-        return runner.run(PoloniexPush)
+        runner = _ApplicationRunner(u"wss://api.poloniex.com:443", u"realm1", extra={'topic': self.topic, 'backFunction': self.backFunction})
+        return runner.run(Session)
 
 
 def main():
     def onOrderBookTrades(*args, **kwargs):
         print("Order Book or Trade update received:", args, kwargs)
 
-    runner = _ApplicationRunner(u"wss://api.poloniex.com:443", u"realm1",
-                                extra={'topic': 'ticker', 'onEvent': onOrderBookTrades})
+    def onTicker(*args, **kwargs):
+        print("Event received:", args)
 
-    runner.run(PoloniexPush)
+    runner = _ApplicationRunner(u"wss://api.poloniex.com:443", u"realm1",
+                                extra={'topic': 'ticker', 'backFunction': onTicker})
+
+    runner.run(Session)
 
 if __name__ == "__main__":
     main()
