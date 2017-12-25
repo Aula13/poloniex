a5d0e8ff92e977d4c78e1c88fa966384ea7b6527
enricobacis <enrico.bacis@gmail.com>
2017-12-25 11:19:37 +0100

    bump to version 0.0.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as README:
     long_description = long_description[long_description.index('Description'):]
 
 setup(name='poloniex',
-      version='0.0.11',
+      version='0.0.12',
       description='Poloniex API client for humans',
       long_description=long_description,
       url='http://github.com/Aula13/poloniex',
