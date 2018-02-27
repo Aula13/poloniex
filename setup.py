8728449c03f45a2a1686633890e1189ec28a1fd3
enricobacis <enrico.bacis@gmail.com>
2018-02-27 13:49:01 +0100

    use pytest-runner<=3.0 for python<2.7 and <=3.3

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
+from sys import version_info
 
 with open('README.rst') as README:
     long_description = README.read()
     long_description = long_description[long_description.index('Description'):]
+
+if (version_info < (2, 7)) or ((3, 0) <= version_info <= (3, 3)):
+    pytest_runner = 'pytest-runner<=3.0'
+else:
+    pytest_runner = 'pytest-runner'
 
 setup(name='poloniex',
       version='0.0.13',
@@ -13,7 +19,7 @@ setup(name='poloniex',
       author_email='enrico.bacis@gmail.com, ciriello.daniele@gmail.com',
       license='MIT',
       packages=['poloniex'],
-      setup_requires=['pytest-runner'],
+      setup_requires=[pytest_runner],
       install_requires=['requests', 'six'],
       tests_require=['pytest', 'responses'],
       keywords='poloniex cryptocurrency cryptocurrencies api client bitcoin'
